2d85b6ec2a905c79f6db32fd548d605a492817b7
Herbert Suantak <108845744+HBMods-OFC@users.noreply.github.com>
2023-04-20 04:00:35 +0530

    Add files via upload

--- settings.js
+++ settings.js
@@ -2,6 +2,19 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
+//global apis
+global.APIs = {
+	zenz: 'https://zenzapi.xyz', 
+}
+//global apikeys
+global.APIKeys = {
+	'https://zenzapi.xyz': '805a6c3fa9', //api keys from zenzi
+}
+
+
+global.Api = new (require('./plugins/neoxrApi.js'))(process.env.API_KEY)
+    
+
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
@@ -14,8 +27,8 @@ global.doc4 = 'application/zip'
 global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
-//owmner v card
-global.owner = ['918416093656','919378043489'] 
+//owner v card
+global.owner = ['918416093656'] 
 global.ownername = "Herbert Suantak"
 global.ytname = "HBMods Channel"
 global.socialm = "herbert70.blogspot.com" 
@@ -23,40 +36,47 @@ global.location = "India,Mizoram;Phuaibuang"
 
 //bot bomdy 
 global.ownernomer = "918416093656" //ur number
-global.premium = ['918416093656'] //ur premium number
-global.botname = 'HBWABot' //ur bot name
-global.linkz = "https://chat.whatsapp.com/C46rhzrso48Cq2rmIrR4Fj" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtube.com/@HBMods_Channel" //ur website to be displayed
-global.botscript = 'https://instagram.com/herbert_suantak2' //script link
-global.themeemoji = "🌚" //ur theme emoji
-global.packname = "Siamted by" //ur sticker watermark packname
-global.author = "HBWABot" //ur sticker watermark author
-global.wm = "HBWABot" //ur watermark
+global.premium = ['918416093656','919378043489']
+global.botname = 'HBWABot'
+global.themeemoji = "♕"
+global.packname = "Siamted by"
+global.author = "HBWABot"
+global.wm = "HBWABot"
+global.reactmoji = "❤️" 
+global.typemenu = 'templateImage'
+global.typemenu2 = 'templateImage'
+
+//Link dahkhawm
+global.linkz = "https://chat.whatsapp.com/C46rhzrso48Cq2rmIrR4Fj"
+global.websitex = "https://youtube.com/@HBMods_Channel"
+global.gbwa1 = "https://herbert70.blogspot.com/2022/04/download-gbwhatsapp-last-version-update.html?m=1"
+global.gbwa2 = "https://herbert70.blogspot.com/2022/12/app-name-gbwhatsapp-pro-version-17.html?m=1"
+global.botscript = 'https://instagram.com/herbert_suantak2'
 
 // Other
 global.sessionName = 'session'
-global.prefa = ['','!','.','#','&']
+global.prefa = ['/','!','.','#','&']
 global.sp = ''
 
 //messages
 global.mess = {
-    success: 'Kha le!',
+    success: 'A hlawhtling e!',
     admin: 'He features hi chu admin te chauh hman thei tur a siam a ni!',
-    botAdmin: 'Admin min nih tir phaw la, chuan ka ti thei ang!',
-    premime: 'Hemi features premium ah ka dah bik hi i hman duh chuan Rent tiin rawn type rawh',
+    botAdmin: 'Admin min nih tir phawt la, chuan ka ti thei ang!',
+    premime: 'Hemi features, premium-ah ka dah bik hi i hman duh chuan owner rawn be rawh',
     owner: 'He features hi a neitu chauh hman thei tura siam a ni!',
-    group: 'Hei chu group ah chauh hman thei tura siam a ni!',
-    private: 'Hei chu private chat ah chauh hman thei tura siam a ni!',
+    group: 'Hei chu group-ah chauh hman thei tura siam a ni!',
+    private: 'Hei chu private chat-ah chauh hman thei tura siam a ni!',
     bot: 'Hei chu kei mah chauh hman thei tur a siam a ni',
     wait: '♲ Nghak lawk rawh..',
-    linkm: 'Khawnge a link🙄?',
-    endLimit: 'Vawiin ah i hman tur i hmang zo ta, darkar 12 ral hunah i hmang leh thei chauh ang',
-    nsfw: 'Nsfw features hi a neitu in a active loh a avngin a hman thei lo a, active turin a siam tu va hrilh rawh',
+    linkm: 'Khawnge a link?',
+    endLimit: 'Vawiinah i hman tur i hmang zo tawh, darkar 12 ral hunah i hmang leh thei chauh ang',
+    nsfw: 'Nsfw features hi a neitu in a activate loh a avangin a hman thei lo a, i hmang duh a nih chuan activate tir turin a siamtu va hriat tir rawh',
 }
 
 //media target
 global.thum = fs.readFileSync("./HBMedia/theme/hbwabot.jpg") //ur thumb pic
-global.log0 = fs.readFileSync("./HBMedia/theme/hbwabot.jpg") //ur logo pic
+global.log0 = fs.readFileSync("./HBMedia/theme/HBWABot2.jpg") //ur logo pic
 global.err4r = fs.readFileSync("./HBMedia/theme/hbwabot.jpg") //ur error pic
 global.thumb = fs.readFileSync("./HBMedia/theme/hbwabot.jpg") //ur thumb pic
 
@@ -85,7 +105,33 @@ global.limitAwal = {
  prem: 'Unlimited',
  free: 70
 }
-
+//url burton
+global.buttonDefault6 = [{
+   urlButton: {
+      displayText: 'GBWhatsApp',
+      url: gbwa1
+   }
+}, {
+   urlButton: {
+      displayText: 'GBWhatsApp Pro',
+      url: gbwa2
+   }
+}, {
+   quickReplyButton: {
+      displayText: 'GBWhatsApp',
+      id: '.gb'
+   }
+}, {
+   quickReplyButton: {
+      displayText: 'YOWhatsApp',
+      id: '.yo'
+   }
+}, {
+   quickReplyButton: {
+      displayText: 'FMWhatsApp',
+      id: '.fm'
+   }
+}]
 //adventure2
 global.emot = {
 role: '🏆',
@@ -101,7 +147,6 @@ uncommon: '🎁',
 mythic: '🗳️',
 legendary: '🗃️',
 pet: '🎁',
-trash: '🗑',
 armor: '👕',
 sword: '⚔️',
 wood: '🪵',
